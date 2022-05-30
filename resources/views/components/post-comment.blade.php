32d876e896c17ff1c91a34e1131be2053822974d
mbm_1607 <muhammadkhan1607@gmail.com>
2022-05-31 02:19:58 +0500

    Add component for comments

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,33 @@
+<article class='flex space-x-4 rounded-xl border bg-gray-100 p-6'>
+  <div class='flex-shrink-0'>
+    <img
+      src='https://i.pravatar.cc/100'
+      alt='Profile Image'
+      loading='lazy'
+      width='60'
+      class='rounded-xl'
+    >
+  </div>
+
+  <div>
+    <header class='mb-4'>
+      <h3 class='font-bold'>
+        John Doe
+      </h3>
+
+      <p class='text-xs'>
+        Posted
+        <time>8 months ago</time>
+      </p>
+    </header>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Libero nisi velit fuga
+      consequuntur esse, minima at tenetur,
+      enim dolorum, facere minus nobis illum
+      excepturi quo molestiae. Aliquid dicta
+      tenetur qui.
+    </p>
+  </div>
+</article>
