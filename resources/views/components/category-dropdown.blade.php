94ed85062e267c6486f669e95ef00ab14aa2c977
mbm_1607 <muhammadkhan1607@gmail.com>
2022-05-31 00:51:40 +0500

    Add pagination & publish paginator

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -8,7 +8,7 @@
   </x-slot>
 
   <x-dropdown-item
-    href='/'
+    href="/?{{ http_build_query(request()->except('category', 'page')) }}"
     :active="request()->routeIs('home')"
   >
     All
@@ -16,7 +16,7 @@
 
   @foreach ($categories as $category)
     <x-dropdown-item
-      href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+      href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
       :active='request()->is("categories/{$category->slug}")'
     >
       {{ $category->name }}
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,5 +3,7 @@
 
   <main class="mx-auto mt-6 max-w-6xl space-y-6">
     <x-posts-grid :posts="$posts" />
+
+    {{ $posts->links() }}
   </main>
 </x-layout>
