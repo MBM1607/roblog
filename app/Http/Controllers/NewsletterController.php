7c964d0e96711ced7be9866b6beae5f29bec81a7
mbm_1607 <muhammadkhan1607@gmail.com>
2023-10-14 00:08:55 +0500

    :bug: Comment out failing mailer subscription

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -7,18 +7,19 @@ use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
 {
-    public function __invoke(Newsletter $newsletter)
-    {
-        request()->validate(['email' => ['required', 'email']]);
-        try {
-            $newsletter->subscribe(request('email'));
-        } catch (Exception $e) {
-            ValidationException::withMessages([
-                'email' => 'This email could not be added to our newsletter'
-            ]);
-        }
-
-        return redirect('/')
-            ->with('success', 'You are now signed up for our newsletter!');
+  public function __invoke(Newsletter $newsletter)
+  {
+    request()->validate(['email' => ['required', 'email']]);
+    try {
+      // TODO Add mailer
+      // $newsletter->subscribe(request('email'));
+    } catch (Exception $e) {
+      ValidationException::withMessages([
+        'email' => 'This email could not be added to our newsletter'
+      ]);
     }
+
+    return redirect('/')
+      ->with('success', 'You are now signed up for our newsletter!');
+  }
 }
